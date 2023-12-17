bec6445fcd68b26632f95fb08c9dcee4da54c813
Yousef <yous3fnoor@gmail.com>
2023-12-17 14:07:32 -0600

    Initial Commit

---
+++ Demo Trying/Demo_TryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_TryingApp.swift
+//  Demo Trying
+//
+//  Created by Yousef Noor on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_TryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
